d8c55134c20048c32a4a847b6d3e443ae1f9a78e
Filipp <git@filip.email>
2020-03-12 16:59:43 +0100

    Added get_data.sh

---
+++ get_data.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://github.com/louismartin/dress-data/raw/master/data-simplification.tar.bz2
+tar -jxvf data-simplification.tar.bz2
+rm data-simplification.tar.bz2
+
+echo "Datasets downloaded."
