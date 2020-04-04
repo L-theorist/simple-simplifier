8dc9bd0c8a8a8d2842c74fe03730317b859f33d3
Filipp <git@filip.email>
2020-04-05 01:04:13 +0200

    updated model to proper Enc-Dec with teacher forcing

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from tensorflow.keras import Input, models, layers, callbacks, optimizers
-
+import nltk
+import numpy as np
+import simplifier
 
 def build_model(vocab_in, vocab_out, length_in, length_out, n_units, use_emb=None, unfreeze_emb=True):
     if use_emb is not None:
@@ -25,29 +27,100 @@ def build_model(vocab_in, vocab_out, length_in, length_out, n_units, use_emb=Non
 
 
     return model
+def build_ed_model(vocab_in, vocab_out, length_in, length_out, n_units, use_emb=None, unfreeze_emb=True):
+    if use_emb is not None:
+        assert n_units==use_emb.shape[-1], "Embedding dimension should match n_units."
+    #Encoder
+    encoder_input = Input(shape=(None,))# vocab_in+1))
+    encoder_emb = layers.Embedding(vocab_in+1,
+                                    n_units,
+                                    #input_length=length_in,
+                                    #embeddings_initializer='lecun_uniform',
+                                    mask_zero=True,
+                                    #trainable=True
+                                    )(encoder_input)
+    encoder_lstm = layers.LSTM(n_units, return_state=True)
+    #encoder_output = layers.RepeatVector(length_out)(encoder_output)
+    encoder_output, state_h, state_c = encoder_lstm(encoder_emb)
+    encoder_states = [state_h, state_c]
+    #Decoder
+    decoder_input = Input(shape=(None,))# vocab_out+1))
+    decoder_emb_layer = layers.Embedding(vocab_out , n_units, mask_zero=True)
+    decoder_emb = decoder_emb_layer(decoder_input)
+    decoder_lstm = layers.LSTM(n_units, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_emb, initial_state=encoder_states)
+    #decoder_output = layers.TimeDistributed(layers.Dense(vocab_out+1, activation='softmax'))(decoder_output)
+
+    decoder_dense = layers.Dense(vocab_out, activation='softmax')
+    decoder_output = decoder_dense(decoder_outputs)
+
+    model = models.Model([encoder_input, decoder_input], decoder_output)
+    if use_emb is not None:
+        model.layers[1].set_weights([use_emb])
+        model.layers[1].trainable = unfreeze_emb
+    # Encoder&Decoder for predictions
+    encoder_model = models.Model(encoder_input, encoder_states)
+    decoder_state_input_h = Input(shape=(n_units,))
+    decoder_state_input_c = Input(shape=(n_units,))
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+    decoder_emb2 = decoder_emb_layer(decoder_input)
+
+    decoder_outputs2, state_h2, state_c2 = decoder_lstm(decoder_emb2, initial_state=decoder_states_inputs)
+    decoder_states2 = [state_h2, state_c2]
+    decoder_outputs2 = decoder_dense(decoder_outputs2)
+
+    decoder_model = models.Model(
+                [decoder_input] + decoder_states_inputs,
+                [decoder_outputs2] + decoder_states2)
 
 
+    return model, encoder_model, decoder_model
 
 
-#
-#
-# model = models.Sequential()
-# model.add(layers.Embedding(          #word embedding
-#     vocabulary_size_source,
-#     #embedding_size,
-#     200,
-#     input_length=sentence_length,
-#     embeddings_initializer='lecun_uniform',
-#     mask_zero=True
-#
-# ))
-# model.add(layers.LSTM(200))#, return_sequences=True))
-# #model.add(layers.LSTM(256)) #
-# model.add(layers.RepeatVector(sentence_length_dst))
-# #model.add(layers.LSTM(256, return_sequences=True)) #
-# model.add(layers.LSTM(200, return_sequences=True))
-# model.add(layers.TimeDistributed(layers.Dense(vocabulary_size_dst+1, activation='softmax')))
-# #model.add(layers.Dense(vocabulary_size_source+1, activation='softmax'))
-#
-# #model.layers[0].set_weights([embedding_matrix])
-# #model.layers[0].trainable = True #False   #freeze
+def decode_sequence(sequence, enc, dec, token_dict, token_dict_reverse=None, length_out=50):
+    if token_dict_reverse is None:
+        token_dict_reverse = dict((index, word) for word, index in token_dict.items())
+        token_dict_reverse[0] = '<pad>'
+    # Encode the input as state vectors.
+    #states_value = encoder_model.predict(sequence)
+    states_value = enc.predict(sequence)
+
+    # Generate empty target sequence of length 1.
+    decoded_sequence = np.zeros((1,1))
+
+    # Populate the first character of target sequence with the start character.
+    decoded_sequence[0, 0] = token_dict['<START>']
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+
+    while not stop_condition:
+        #output_tokens, h, c = decoder_model.predict([decoded_sequence] + states_value)
+        output_tokens, h, c = dec.predict([decoded_sequence] + states_value)
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_word = token_dict_reverse[sampled_token_index]
+        decoded_sentence += ' '+sampled_word
+
+        # Exit condition: either hit max length or find stop token.
+        if (sampled_word == '<END>' or len(decoded_sentence) > length_out):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        decoded_sequence = np.zeros((1,1))
+        decoded_sequence[0, 0] = sampled_token_index
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
+
+
+def bleu_score(ref, hyp):
+
+    ref = simplifier.tokenize(ref[0])
+    BLEUscore = nltk.translate.bleu_score.sentence_bleu([ref], hyp, weights=(1, 1, 1, 1))
+    return np.round(BLEUscore*100,1)
--- simplifier.py
+++ simplifier.py
@@ -6,17 +6,22 @@ from tensorflow.keras import preprocessing
 from unicodedata import normalize
 import collections
 from operator import itemgetter
-
-def build_word2index(word_list):
+from tensorflow.keras.utils import to_categorical
+import string
+def build_word2index(word_list, add_toc=False):
     """
     Takes a list of (unique!) word tokens. Returns a word-index dictionary, last index is assigned to '<UNK>'.
+    If add_toc=True, adds a start/end tokens.
     """
     tokenizer_dict = {}
     i = 1
-    for word in word_list:
+    for word in set(word_list):
         tokenizer_dict[word] = i
         i += 1
     tokenizer_dict["<UNK>"] = i
+    if add_toc:
+        tokenizer_dict['<START>'] = i+1
+        tokenizer_dict['<END>'] = i+2
     return tokenizer_dict
 
 def tokenize(clean_text):
@@ -46,16 +51,17 @@ def build_vocabulary(token_list, threshold=1, with_freq=True):
 
 def clean(sentences):
     """
-    Takes the corpus, decodes into UTF-8, tokenizes on white space, puts everything in lowercase, removes numbers (!?)
+    Takes the corpus, decodes into UTF-8, tokenizes on white space, puts everything in lowercase, removes punctuation.
     Returns string.
     """
-
+    translator = str.maketrans('', '', string.punctuation)
     clean_txt = ''
     for line in sentences:
 
         line = normalize('NFD', line).encode('ascii', 'ignore')
         line = line.decode('UTF-8')
         # tokenize on white space
+        line = line.translate(translator)
         line = line.split()
         # convert to lowercase
         line = [word.lower() for word in line]
@@ -64,18 +70,19 @@ def clean(sentences):
         # remove non-printable chars form each token
         #line = [re_print.sub('', w) for w in line]
         # remove tokens with numbers in them
-        line = [word for word in line if word.isalpha()]
+        line = [word for word in line if (word.isalpha() or word.isnumeric())]
         # store as string
         clean_txt += (' '.join(line))
     return clean_txt
 
 
 
-def tokenizer(line):
+def tokenizer(line, add_toc=False):
     """
     Uses nltk RefexpTokenizer to tokenize wrt alphanumerics, normalizes, decodes UTF8, ensures lower case.
     Takes a Sentence(s) string as input.
     Returns a list of token words.
+    If add_toc=True, adds start and end tokens.
     """
 
     tokenizer = RegexpTokenizer(r'\w+')
@@ -83,10 +90,12 @@ def tokenizer(line):
     line = line.decode('UTF-8')
     tokenized_line = tokenizer.tokenize(line)
     tokenized_line = [word.lower() for word in tokenized_line]
+    if add_toc:
+        tokenized_line = ['<START>'] + tokenized_line + ['<END>']
     return tokenized_line
 
 
-def encoder(tokenized_line, token_dict, seq_len):
+def encode(tokenized_line, token_dict, seq_len, pad=True):
     """
     Takes a list of word tokens and translates it into a list of word indices using a dictionary.
     Returns an array of token words, of shape (?, seq_len). Post padding and post truncating is applied.
@@ -95,11 +104,12 @@ def encoder(tokenized_line, token_dict, seq_len):
     encoded_line = []
     for word in tokenized_line:
         encoded_line.append(token_dict.get(word, token_dict["<UNK>"]))
-    encoded_line = preprocessing.sequence.pad_sequences(
-    [encoded_line],
-    maxlen=seq_len,
-    padding="post",
-    truncating="post")
+    if pad:
+        encoded_line = preprocessing.sequence.pad_sequences(
+        [encoded_line],
+        maxlen=seq_len,
+        padding="post",
+        truncating="post")
 
     return encoded_line
 
@@ -111,7 +121,7 @@ def one_hot_encode_output(encoded_sentences, sentence_length_dst=50,vocabulary_s
     decoder_targets_one_hot = np.zeros((
             number_samples,
             sentence_length_dst,
-            vocabulary_size_dst+1  # hm... why?
+            vocabulary_size_dst  # hm... why?
         ))
     for i, sentence in enumerate(curr):
         for t, word in enumerate(sentence):
@@ -130,6 +140,11 @@ def one_hot_encode_output(encoded_sentences, sentence_length_dst=50,vocabulary_s
 #             for t, word in enumerate(sentence):
 #                 decoder_targets_one_hot[i, t, word] = 1
 #         yield decoder_targets_one_hot
+
+def one_hot_encode_ed(sent, vocabulary_size):
+    encoded = to_categorical(sent, num_classes=vocabulary_size)
+    return encoded
+
 
 def generator(X_data, Y_data, batch_size, samples=30000, shf=True, onehot=True):
     """generates data for the fit_generator
@@ -154,6 +169,46 @@ def generator(X_data, Y_data, batch_size, samples=30000, shf=True, onehot=True):
             y_batch = one_hot_encode_output(y_batch)
         counter += 1
         yield x_batch,y_batch
+
+        #restart counter to yield data in the next epoch as well
+        if counter >= number_of_batches:
+            counter = 0
+
+def generator_ed(X_data, Y_data, batch_size, vocab_in, vocab_out, samples=30000, shf=True, onehot_dst=True, onehot_src=True):
+    """generates data for the fit_generator of an encoder-decoder model
+    input X_data, Y_data, batch_size, samples=30000
+    shf=False returns the first #samples of X_data, Y_data; otherwise shuffles first.
+    onehot_src=True applies one hot encoding to x_batch
+    onehot_dst=True applies one hot encoding to y_batch
+    yields x_batch, x2_batch, y_batch"""
+    #from sklearn.utils import shuffle
+    if shf:
+        x_data, y_data = shuffle(X_data, Y_data, n_samples=samples)
+    else:
+        x_data = X_data[0:samples]
+        y_data = Y_data[0:samples]
+    samples_per_epoch = x_data.shape[0]
+    number_of_batches = samples_per_epoch/batch_size
+    counter=0
+    pad = np.zeros((batch_size, 1))
+    pad_end = np.zeros((batch_size, 1))
+    # pad = np.zeros((batch_size,))
+    # pad_end = np.zeros((batch_size,))
+    while True:
+        x_batch = np.array(x_data[batch_size*counter:batch_size*(counter+1)])#.astype('float32')
+        y_batch = np.array(y_data[batch_size*counter:batch_size*(counter+1)])#.astype('float32')
+        x2_batch = np.concatenate((pad, y_batch[:,:-1]), axis=1)
+        #x2_batch = np.concatenate((pad, y_batch), axis=0)
+        y_batch = np.concatenate((y_batch[:,1:], pad_end), axis=1)
+        #y_batch = np.concatenate((y_batch, pad_end), axis=0)
+        if onehot_src:
+            x_batch = one_hot_encode_ed(x_batch, vocab_in)
+        if onehot_dst:
+            y_batch = one_hot_encode_ed(y_batch, vocab_out)
+            #x2_batch = one_hot_encode_ed(x2_batch, vocab_out+3)
+
+        counter += 1
+        yield [x_batch, x2_batch], y_batch
 
         #restart counter to yield data in the next epoch as well
         if counter >= number_of_batches:
